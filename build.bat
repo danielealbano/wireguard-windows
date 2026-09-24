422703e9d2635d17a2516dbc9e7151806130b610
Daniele Salvatore Albano <d.albano@gmail.com>
2026-09-24 17:40:37 +0200

    feat(build): build wg.exe from the wireguard-tools fork
    Pin the danielealbano/wireguard-tools fork at 68b49a93, whose wg.exe
    shows and configures the WebSocket keys of userspace tunnels.

--- build.bat
+++ build.bat
@@ -20,7 +20,7 @@ if exist .deps\prepared goto :render
 	call :download imagemagick.zip https://download.wireguard.com/windows-toolchain/distfiles/ImageMagick-7.0.8-42-portable-Q16-x64.zip 584e069f56456ce7dde40220948ff9568ac810688c892c5dfb7f6db902aa05aa "convert.exe colors.xml delegates.xml" || goto :error
 	rem Mirror of https://sourceforge.net/projects/ezwinports/files/make-4.2.1-without-guile-w32-bin.zip
 	call :download make.zip https://download.wireguard.com/windows-toolchain/distfiles/make-4.2.1-without-guile-w32-bin.zip 30641be9602712be76212b99df7209f4f8f518ba764cf564262bc9d6e4047cc7 "--strip-components 1 bin" || goto :error
-	call :download wireguard-tools.zip https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-06a99cce2c9998f53eb30d2f258a9e5ff286445b.zip b7a73e027cee3127f3cccba8ad3a08ea61ccd42d3ea5c28c548a8e0ec9e10cf6 "--exclude wg-quick --strip-components 1" || goto :error
+	call :download wireguard-tools.zip https://github.com/danielealbano/wireguard-tools/archive/68b49a933d178aaadf2a2e2becbb0fc0d864eb3a.zip 4ef3dd8fbc618c990b621c08462eaee0fbe015de015f4b564241d2cd303d0b27 "--exclude wg-quick --strip-components 1" || goto :error
 	call :download wintun.zip https://www.wintun.net/builds/wintun-0.14.1.zip 07c256185d6ee3652e09fa55c0b673e2624b565e02c4b9091c79ca7d2f24ef51 || goto :error
 	call :download wireguard-nt.zip https://download.wireguard.com/wireguard-nt/wireguard-nt-1.1.zip dceb30a9bc4be48cce0f74160fc88a585a2c2627366e8f846fc6658f9038dace || goto :error
 	copy /y NUL prepared > NUL || goto :error
